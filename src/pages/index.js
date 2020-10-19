9141936745a54f23710a146512c94d4d7248c67a
C.Todd Lombardo <iamctodd@users.noreply.github.com>
2020-10-18 20:08:40 -0400

    update style

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
 import HeroSection from "../components/sections/HeroSection"
+import { Link } from "gatsby"
 
 function IndexPage() {
   return (
     <Layout>
       <SEO title="Home" />
       <HeroSection />
+      <Link to="/page-2">page2</Link>
     </Layout>
   )
 }
